60d279f9a84df4ec559e763c2816c49b18b511ee
Mikful <40219831+Mikful@users.noreply.github.com>
2019-06-18 14:20:12 -0500

    Updated script.js

--- js/script.js
+++ js/script.js
@@ -3,17 +3,14 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
 
+/* Create the quotes object array:
+Each quote object has a `quote` and `source` property.
+In addition, `citation` and `year` properties are given where known.
+HTML line breaks are given for poetic quotes where necessary using  <br />.
+console.log() logs the array of quotes to the console. 
+*/
 
-/*** 
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-  Add the `citation` property to at least one object in the array.
-  Add the `year` property to at least one object in the array.
-  Use console.log() to log your array of quotes to the console.
-***/
 
 let quotes = [
   {quote: 'In ancient times having power meant having access to data. Today having power means knowing what to ignore.', source: 'Yuval Noah Harari', citation: 'Homo Deus' },
@@ -22,7 +19,7 @@ let quotes = [
   {quote: 'After silence, that which comes nearest to expressing the inexpressible is music.', source: 'Aldous Huxley',  citation: 'Music at Night and Other Essays' },
   {quote: 'Anyone who has the power to make you believe absurdities has the power to make you commit injustices.', source: 'Voltaire'},
   {quote: 'There is no right and wrong, but right is right and wrong is wrong.', source: 'Suzuki Roshi'},
-  {quote: "He who binds to himself a Joy, Does the winged life destroy; He who kisses the Joy as it flies,Lives in Eternity's sunrise.", source: 'William Blake', citation: 'Eternity'},
+  {quote: "He who binds to himself a Joy,<br /> Does the winged life destroy;<br /> He who kisses the Joy as it flies,<br /> Lives in Eternity's sunrise.", source: 'William Blake', citation: 'Eternity'},
   {quote: 'To learn how to die is to learn how to live.', source: 'Sogyal Rinpoche', citation: 'The Tibetan Book of Living and Dying'},
   {quote: 'In the thick of the forest is where you will find your freedom.', source: 'Buddha'},
   {quote: 'The real voyage of discovery consists not in seeing new landscapes but in having new eyes.', source: 'Marcel Proust'},
@@ -30,53 +27,54 @@ let quotes = [
   {quote: "Fear is the cheapest room in the house. I'd rather see you in better living conditions.", source: 'Hafiz'},
   {quote: 'Construction and destruction alike satisfy the will to power, but construction is more difficult as a rule, and therefore gives more satisfaction to the person who can achieve it.', source: 'Bertrand Russell'},
   {quote: 'If you think something is ugly, look harder. Ugliness is just a failure of seeing.', source: 'Matt Haig', citation: 'The Humans'},
-  {quote: 'Happy the man, and happy he alone, He who can call today his own: He who, secure within, can say: "Tomorrow, do thy worst, for I have lived today."', source: 'Horace', year: '30 B.C.'},
+  {quote: 'Happy the man, and happy he alone,<br /> He who can call today his own:<br /> He who, secure within, can say:<br /> "Tomorrow, do thy worst, for I have lived today."', source: 'Horace', year: '30 B.C.'},
   {quote: 'Questions you cannot answer are usually far better for you than questions you cannot question.', source: 'Yuval Noah Harai', citation: '21 Lessons For The 21st Century'},
   {quote: "As a bee takes the essence of a flower and pollen without destroying it's beauty or perfume, so the wise wander freely in this life, carrying only blessings.", source: 'Buddha'},
-  {quote: 'I know that most men, including those at ease with problems of the greatest complexity, can seldom accept even the simplest and most obvious truth if it be such as would oblige them to admit the falsity of conclusions which they have delighted in explaining to colleagues, which they have proudly taught to others, and which they have woven, thread by thread, into the fabric of their lives.', source: 'Leo Tolstoy'},
   {quote: "Anger - with it's poisoned source and it's honeyed tip.", source: 'Buddha'},
-  {quote: 'The mere fact of thinking that a good may be acquired or an evil avoided is sufficient to produce the desire for this to come to pass. But when, over and above this, we consider whether our desire is likely to be satisfied or not, the idea that it is likely arouses hope in us, and the idea that it is unlikely arouses fear, of which one variety is jealousy.', source: 'Descartes', citation: 'The Passions of the Soul'}
 ];
 console.log(quotes);
 
 
-/***
-  Create the `getRandomQuote` function to:
-   - Create a variable to store a random number 
-   - Call the random number to `return` a random quote object from the `quotes` array.
-***/
+/* Create the random quote generator function:
+- Function creates a random integer between 1 and the maximum index number of quotes object array (when supplied the argument 'quotes.length' in the call) and stores it in a variable
+- The random number variable is called to `return` a random quote object from the `quotes` array. 
+*/
 
-function getRandomQuote() {
-  randomNum = Math.floor((Math.random() * quotes.length) + 1);
-  randomQuote = quotes[randomNum];
+function getRandomQuote(max) { 
+  let randomNum = Math.floor(Math.random() * Math.floor(max));
+  console.log(randomNum);
+  let randomQuote = quotes[randomNum];
   return randomQuote;
 };
 
 
-
-
-/***
-  Create the `printQuote` function to: 
-   - Call the `getRandomQuote` function and assign it to a variable.
-   - Create a variable for the HTML string and set it equal to an empty string.
-   - Use the HTML template in the instructions or the markup in the index.html file, AND 
-     the random quote variable to build your HTML string.
-   - Add the quote and source section to the HTML string.
-   - Use an if statement to check for the citation property before adding it to the HTML string.
-   - Use an if statement to check for the year property before adding it to the HTML string.
-   - Don't forget to close that final `p` tag.
-   - Set the `innerHTML` of the `quote-box` div to the HTML string. 
-***/
-
-// Print
-// This uses a node to dynamically update the HTML within the page with the targeted id */
-function print(message) {
-  var outputDiv = document.getElementById('quote-box'); /* Look at html and locate id 'quote-box and change dynamically */
+/* Print Function
+ - This uses a node to dynamically update the HTML within the page at the targeted id with two parameters:
+  - 'message' - the created string to print to HTML
+  - 'htmlID' - the HTML div id that we want to update
+ */
+function print(message, htmlID) {
+  let outputDiv = document.getElementById(htmlID); /* Look at HTML and locate element by id argument */
   outputDiv.innerHTML = message; /* change HTML within the targetd id*/
 }
 
+
+/* Create the 'printQuote' function:
+- Call the 'getRandomQuote' function, provide the quotes.length argument and assign returned quote array to a variable.
+- Create an empty HTML string 
+- Initialise an empty HTMl string
+- Create the full HTML string from the quote properties:
+  - 'quote' and 'source' properties of the quote object
+  - if the 'citation' property exists add it to the HTML string
+  - if the 'year' property exists add it to the HTML string
+- Call the 'print' function and pass arguments: 
+  - "HTML" to print the created HTML string 
+  - "'quote-box'" to target the desired HTMl element by its assigned id
+*/
+
 function  printQuote() {
-  randomQuote = getRandomQuote();
+  let randomQuote = getRandomQuote(quotes.length);
+  let HTML = '';
   HTML = '<p class="quote">' + randomQuote.quote + '</p>' +
   '<p class="source">' + randomQuote.source ;
   if (randomQuote.hasOwnProperty('citation')) {
@@ -86,19 +84,14 @@ function  printQuote() {
     HTML += '<span class="year">' + randomQuote.year + '</span>' 
   };
   HTML+= '</p>';
-  print(HTML);
+  print(HTML, 'quote-box');
 };
 
 
+/* 
+When the "Show another quote" button is clicked, the event listener 
+below will be triggered, calling the `printQuote` function. 
+*/
 
-/***
-  When the "Show another quote" button is clicked, the event listener 
-  below will be triggered, and it will call, or "invoke", the `printQuote` 
-  function. So do not make any changes to the line of code below this 
-  comment.
-***/
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
